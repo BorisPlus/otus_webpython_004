5579564b9878b9dd880dfd7cd8cfd7c05689e455
developer <boris-plus@mail.ru>
2018-08-29 21:35:28 +0300

    папка db и сама бд не хранится в проекте, она будет создаваться при первом запуске приложения

--- kiosk/config.py
+++ kiosk/config.py
@@ -6,6 +6,9 @@ app_base_path = os.path.dirname(os.path.abspath(__file__))
 app_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(app_name)
 app.secret_key = b'012465789'
-db_path = '%s.db' % os.path.join(app_base_path, 'db', app_name)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_path
+db_folder = os.path.join(app_base_path, 'db')
+if not db_folder:
+    os.mkdir(db_folder)
+db_file = '%s.db' % os.path.join(db_folder, app_name)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_file
 db = SQLAlchemy(app)
