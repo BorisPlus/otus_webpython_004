63867538ea768b7daed593baa5068905149e2fa6
developer <boris-plus@mail.ru>
2018-08-30 08:40:42 +0300

    создание каталога БД при его отсутствии

--- kiosk/config.py
+++ kiosk/config.py
@@ -7,7 +7,7 @@ app_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(app_name)
 app.secret_key = b'012465789'
 db_folder = os.path.join(app_base_path, 'db')
-if not db_folder:
+if not os.path.exists(db_folder):
     os.mkdir(db_folder)
 db_file = '%s.db' % os.path.join(db_folder, app_name)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_file
